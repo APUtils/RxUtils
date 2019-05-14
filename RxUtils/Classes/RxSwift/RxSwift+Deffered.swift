cfbd23c2a07866f3389e7da9cef0d1d40488a13e
Anton Plebanovich <anton.plebanovich@itechart-group.com>
2019-05-14 15:16:29 +0300

    - Documentation update

--- RxUtils/Classes/RxSwift/RxSwift+Deffered.swift
+++ RxUtils/Classes/RxSwift/RxSwift+Deffered.swift
@@ -14,8 +14,11 @@ import RxSwift
 
 public extension ObservableType {
     
-    /// Evaluates startWith expression on subscribe and pass it as element to a sequence.
-    /// Do not emit anything if evaluates to nil.
+    /// Prepends and evaluates element expression to an observable sequence on subscription.
+    /// Do not emit anything if element expression evaluates to `nil`.
+    /// - seealso: [startWith operator on reactivex.io](http://reactivex.io/documentation/operators/startwith.html)
+    /// - parameter element: Element to prepend to the specified sequence on subscription or `nil`.
+    /// - returns: The source sequence prepended with the specified element if it is not `nil`.
     func startWithDeferred(_ element: @escaping @autoclosure () -> E?) -> Observable<E> {
         return Observable.create {
             if let element = element() {
