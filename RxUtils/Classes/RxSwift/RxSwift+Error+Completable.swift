7136829e0601d403d8a2e2d33ef76b36c1e61980
Anton Plebanovich <anton.plebanovich@gmail.com>
2020-04-17 16:23:03 +0300

    - Completable .catchErrorJustComplete()

---
+++ RxUtils/Classes/RxSwift/RxSwift+Error+Completable.swift
@@ -0,0 +1,18 @@
+//
+//  RxSwift+Error+Completable.swift
+//  RxUtils
+//
+//  Created by Anton Plebanovich on 4/17/20.
+//  Copyright © 2020 Anton Plebanovich. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+public extension PrimitiveSequence where Trait == RxSwift.CompletableTrait, Element == Never {
+    
+    /// Just completes a sequence on an error.
+    func catchErrorJustComplete() -> Completable {
+        catchError { _ in Completable.empty() }
+    }
+}
