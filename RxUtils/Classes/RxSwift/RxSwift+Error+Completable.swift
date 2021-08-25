d722599ba486a183b3da12edf793cf69f12c42fc
Anton Plebanovich <anton.plebanovich@gmail.com>
2021-08-26 01:44:07 +0300

    - Completable .mapErrorTo - Completable .mapError

--- RxUtils/Classes/RxSwift/RxSwift+Error+Completable.swift
+++ RxUtils/Classes/RxSwift/RxSwift+Error+Completable.swift
@@ -11,6 +11,18 @@ import RxSwift
 
 public extension PrimitiveSequence where Trait == RxSwift.CompletableTrait, Element == Never {
     
+    /// Maps error into other error.
+    /// - parameter transform: A transform function to apply to source error.
+    func mapError(_ transform: @escaping (Error) -> Error) -> Completable {
+        return self.catch { error -> Completable in .error(transform(error)) }
+    }
+    
+    /// Maps error into other error.
+    /// - parameter error: Error to transform to.
+    func mapErrorTo(_ error: Error) -> Completable {
+        return self.catch { error -> Completable in .error(error) }
+    }
+    
     /// Just completes a sequence on an error.
     func catchErrorJustComplete() -> Completable {
         self.catch { _ in Completable.empty() }
