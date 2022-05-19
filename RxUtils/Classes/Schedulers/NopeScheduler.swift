e9edb127649f19c652247b5e1d00c9465ef5dd8b
Anton Plebanovich <anton.plebanovich@gmail.com>
2022-05-19 09:33:25 +0300

    - NopeScheduler

---
+++ RxUtils/Classes/Schedulers/NopeScheduler.swift
@@ -0,0 +1,16 @@
+//
+//  NopeScheduler.swift
+//  Pods
+//
+//  Created by Anton Plebanovich on 19.05.22.
+//  Copyright © 2022 Anton Plebanovich. All rights reserved.
+//
+
+import RxSwift
+
+/// Just performs operation immediatelly instead of scheduling it somehow.
+public final class NopeScheduler: ImmediateSchedulerType {
+    public func schedule<StateType>(_ state: StateType, action: @escaping (StateType) -> Disposable) -> Disposable {
+        return action(state)
+    }
+}
