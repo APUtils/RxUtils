88ce00fd2672dd69cdd989520a433742b205075e
Anton Plebanovich <anton.plebanovich@gmail.com>
2023-08-09 19:24:49 +0600

    - [ControlEvent] `merge` operator

---
+++ RxUtils/Classes/Operators/ControlEvent.swift
@@ -0,0 +1,45 @@
+//
+//  ControlEvent.swift
+//  Pods
+//
+//  Created by Anton Plebanovich on 9.08.23.
+//  Copyright © 2023 Anton Plebanovich. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+
+extension ControlEvent {
+    /// Merges elements from all control event sequences from collection into a single control event sequence.
+    ///
+    /// - seealso: [merge operator on reactivex.io](http://reactivex.io/documentation/operators/merge.html)
+    ///
+    /// - parameter sources: Collection of control event sequences to merge.
+    /// - returns: The control event sequence that merges the elements of the control event sequences.
+    public static func merge<Collection: Swift.Collection>(_ sources: Collection) -> ControlEvent<Element> where Collection.Element == ControlEvent<Element> {
+        let observableSources = sources.map { $0.asObservable() }
+        return ControlEvent(events: Observable.merge(observableSources))
+    }
+    
+    /// Merges elements from all control event sequences from collection into a single control event sequence.
+    ///
+    /// - seealso: [merge operator on reactivex.io](http://reactivex.io/documentation/operators/merge.html)
+    ///
+    /// - parameter sources: Array of control event sequences to merge.
+    /// - returns: The control event sequence that merges the elements of the control event sequences.
+    public static func merge(_ sources: [ControlEvent<Element>]) -> ControlEvent<Element> {
+        let observableSources = sources.map { $0.asObservable() }
+        return ControlEvent(events: Observable.merge(observableSources))
+    }
+    
+    /// Merges elements from all control event sequences from collection into a single control event sequence.
+    ///
+    /// - seealso: [merge operator on reactivex.io](http://reactivex.io/documentation/operators/merge.html)
+    ///
+    /// - parameter sources: Collection of control event sequences to merge.
+    /// - returns: The control event sequence that merges the elements of the control event sequences.
+    public static func merge(_ sources: ControlEvent<Element>...) -> ControlEvent<Element> {
+        let observableSources = sources.map { $0.asObservable() }
+        return ControlEvent(events: Observable.merge(observableSources))
+    }
+}
