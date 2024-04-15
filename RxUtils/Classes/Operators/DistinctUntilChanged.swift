b59abd0453b83b9c817af6788a8efe53dd78ee98
Anton Plebanovich <anton.plebanovich@gmail.com>
2024-04-15 19:11:09 +0600

    - [ObservableType, ObservableConvertibleType] `distinctUntilChanged()` for tuple of 3 and 4 equatable elements

--- RxUtils/Classes/Operators/DistinctUntilChanged.swift
+++ RxUtils/Classes/Operators/DistinctUntilChanged.swift
@@ -16,6 +16,16 @@ public extension ObservableConvertibleType {
         asObservable()
             .distinctUntilChanged()
     }
+    
+    func distinctUntilChanged<T: Equatable, Y: Equatable, U: Equatable>() -> Observable<Element> where Element == (T, Y, U) {
+        asObservable()
+            .distinctUntilChanged()
+    }
+    
+    func distinctUntilChanged<T: Equatable, Y: Equatable, U: Equatable, I: Equatable>() -> Observable<Element> where Element == (T, Y, U, I) {
+        asObservable()
+            .distinctUntilChanged()
+    }
 }
 
 // ******************************* MARK: - ObservableType
@@ -25,6 +35,23 @@ public extension ObservableType {
     func distinctUntilChanged<T: Equatable, Y: Equatable>() -> Observable<Element> where Element == (T, Y) {
         distinctUntilChanged { lhs, rhs in
             lhs.0 == rhs.0 && lhs.1 == rhs.1
+        }
+    }
+    
+    func distinctUntilChanged<T: Equatable, Y: Equatable, U: Equatable>() -> Observable<Element> where Element == (T, Y, U) {
+        distinctUntilChanged { lhs, rhs in
+            lhs.0 == rhs.0
+            && lhs.1 == rhs.1
+            && lhs.2 == rhs.2
+        }
+    }
+    
+    func distinctUntilChanged<T: Equatable, Y: Equatable, U: Equatable, I: Equatable>() -> Observable<Element> where Element == (T, Y, U, I) {
+        distinctUntilChanged { lhs, rhs in
+            lhs.0 == rhs.0
+            && lhs.1 == rhs.1
+            && lhs.2 == rhs.2
+            && lhs.3 == rhs.3
         }
     }
 }
