199530a99a836d67cc859e7a5fbdf476b02a49cd
Anton Plebanovich <anton.plebanovich@gmail.com>
2024-08-25 13:20:48 +0600

    - [BackgroundSafeTimer] Support for Driver

--- RxUtils/Classes/Operators/BackgroundSafeTimer.swift
+++ RxUtils/Classes/Operators/BackgroundSafeTimer.swift
@@ -133,3 +133,58 @@ public extension ObservableType where Element == Void {
             }
     }
 }
+
+// ******************************* MARK: - SharedSequence
+
+public extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, Element == Void {
+    
+    /**
+     Returns an observable sequence that periodically produces a value after the specified initial relative due time has elapsed, using the specified scheduler to run timers.
+     
+     It works around the issue of broken timer in the background. Please check specs for more info.
+     
+     - seealso: [timer operator on reactivex.io](http://reactivex.io/documentation/operators/timer.html)
+     
+     - parameter dueTime: Relative time at which to produce the first value.
+     - parameter period: Period to produce subsequent values.
+     - parameter scheduler: Scheduler to run timers on.
+     - parameter possiblyWakedUp: There is no system event for application wake up from suspension nor go to suspension so we have to guess. By default only the app leave from the background is checked but if your app works in the background you should add more: push location, push notification or background fetch.
+     - parameter timerScheduler: Just for tests.
+     - returns: A driver that produces a value after due time has elapsed and then each period.
+     */
+    static func backgroundSafeTimer(
+        _ dueTime: RxSwift.RxTimeInterval,
+        period: RxSwift.RxTimeInterval?,
+        possiblyWakedUp: Observable<Void> = UIApplication.shared.rx.didLeaveBackground.subscribe(on: ConcurrentMainScheduler.instance),
+        file: String = #file,
+        function: String = #function,
+        line: UInt = #line,
+        timerScheduler: @escaping (
+            _ dueTime: RxSwift.RxTimeInterval,
+            _ period: RxSwift.RxTimeInterval?
+        ) -> Driver<Int> = Driver<Int>.timer(_:period:)
+    ) -> Driver<Void> {
+        
+        Observable.backgroundSafeTimer(
+            dueTime,
+            period: period, 
+            scheduler: SharingStrategy.scheduler,
+            possiblyWakedUp: possiblyWakedUp,
+            file: file, 
+            function: function,
+            line: line,
+            timerScheduler: { dueTime, period, _ in
+                timerScheduler(dueTime, period).asObservable()
+            }
+        )
+        .asDriver(onErrorDriveWith: .empty())
+    }
+}
+
+public extension SharedSequenceConvertibleType where SharingStrategy == DriverSharingStrategy, Element: RxAbstractInteger {
+    
+    static func timer(_ dueTime: RxTimeInterval, period: RxTimeInterval?) -> Driver<Element> {
+        Observable<Element>.timer(dueTime, period: period, scheduler: SharingStrategy.scheduler)
+            .asDriver(onErrorDriveWith: .empty())
+    }
+}
